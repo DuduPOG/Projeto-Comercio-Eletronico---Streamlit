ee4bbe6f307ea8085861b922f2c9bcbe15ad229f
DuduPOG <eduardo.aguiar@escolar.ifrn.edu.br>
2025-07-14 18:41:34 +0000

    Corrigindo o menu admin e atualizando o diagrama

--- Projeto/index.py
+++ Projeto/index.py
@@ -84,7 +84,7 @@ class IndexUI:
             st.sidebar.write(f"Bem vindo(a), " + st.session_state["cliente_nome"])
             if admin:
                 IndexUI.menu_admin()
-            if st.session_state["cliente_nome"] == "eduardo":
+            elif st.session_state["cliente_nome"] == "eduardo":
                 IndexUI.menu_entregador()
             else:
                 IndexUI.menu_cliente()
